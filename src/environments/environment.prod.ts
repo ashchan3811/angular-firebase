df9b1f491798423b2e484ccf40025f23dd05b4c5
Ashwani Kumar <kumar.ashwani@live.in>
2017-12-02 22:33:25 +0530

    -

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBih3EAhW8gMGkHSezsuITf7Y1foYVXUB0',
+    authDomain: 'angular-firebase-3811.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-3811.firebaseio.com',
+    projectId: 'angular-firebase-3811',
+    storageBucket: 'angular-firebase-3811.appspot.com',
+    messagingSenderId: '1046738808452'
+  }
 };
